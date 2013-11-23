6368225565c18d85dc92eefc61ad44e294e98711
Thomas Kokemoor <thomas.kokemoor@gmail.com>
2013-11-23 14:10:30 -0600

    Completed matching function

--- RegexState.hs
+++ RegexState.hs
@@ -3,16 +3,20 @@ module RegexState where
 import Control.Applicative
 import Control.Arrow
 import Control.Monad
+import Control.Monad.Fix
 
 type RegexError = String
 type Failing a  = Either RegexError a
 
--- basically every parse error comes down to this
-errParen :: Failing a
+-- basically every parse error comes down to these
+errParen, errAlternate, errRepeat, errRange :: Failing a
 errParen = Left "Unbalanced parentheses"
+errAlternate = Left "Empty alternation"
+errRepeat = Left "Empty repetition"
+errRange = Left "Invalid repetition range"
 
-(<$$>) :: Either a c -> (a -> b) -> Either b c
-Left  x <$$> f = Left (f x)
+(<$$>) :: Either String b -> String -> Either String b
+Left  x <$$> s = Left (x ++ " (" ++ s ++ ")")
 Right r <$$> _ = Right r
 
 data Atom
@@ -54,7 +58,7 @@ shuntRegexRec :: String -> ShuntYard -> Failing PostfixRegex
 shuntRegexRec "" yard = closeYard yard
 shuntRegexRec input yard = do
   (t, rest) <- tokenize input
-  shuntRegexRec rest <=< (\f -> f yard) $ case t of
+  shuntRegexRec rest <=< ($ yard) $ case t of
     LParen      -> restack
     RParen      -> destack
     Alternative -> alternate
@@ -81,7 +85,7 @@ closeYard yard = do
   (stack, output) <- destack yard
   case stack of
     [] -> pure $ reverse output
-    _  -> errParen <$$> (++" (closeYard)")
+    _  -> errParen <$$> "closeYard"
   
 
 restack :: ShuntYard -> Failing ShuntYard
@@ -99,7 +103,7 @@ destack (stack, output) = case stack of
                            ++ replicate (natom cell - 1) Catenate
                            ++ output
                          )
-  _              -> errParen <$$> (++" (destack)")
+  _              -> errParen <$$> "destack"
 
 alternate :: ShuntYard -> Failing ShuntYard
 alternate (stack, output) = case stack of
@@ -107,11 +111,119 @@ alternate (stack, output) = case stack of
                     ,    replicate (natom cell - 1) Catenate
                       ++ output
                     )
-  _         -> errParen <$$> (++" (alternate)")
+  _         -> errParen <$$> "alternate"
 
 pushAtom :: Atom -> ShuntYard -> Failing ShuntYard
 pushAtom a (stack, output) = case stack of
   cell:rest -> pure ( cell { natom = natom cell + 1 } : rest
                     , a:output
                     )
-  _         -> errParen <$$> (++" (pushAtom)")
+  _         -> errParen <$$> "pushAtom"
+
+
+data RegexState
+  = Match    Char RegexState
+  | MatchAny      RegexState
+  | Branch        RegexState RegexState
+  | Success
+
+instance Show RegexState where
+  show (Match  c _) = 'L':show c
+  show (MatchAny _) = "Dot"
+  show (Branch _ _) = "Branch"
+  show (Success   ) = "Success"
+  
+
+type MachineFragment = RegexState -> RegexState
+type FragmentStack   = [MachineFragment]
+
+fromPostfix :: PostfixRegex -> Failing RegexState
+fromPostfix atoms = fromPostfixRec atoms []
+
+fromPostfixRec :: PostfixRegex -> FragmentStack -> Failing RegexState
+fromPostfixRec [] fragments = case fragments of
+  [f] -> pure (f Success)
+  _   -> errParen <$$> "fromPostfixRec"
+fromPostfixRec (a:as) fragments =
+  fromPostfixRec as <=< ($ fragments) $ case a of
+    Literal c     -> pure . (Match c:)
+    Any           -> pure . (MatchAny:)
+    Alternative   -> alternateMachine
+    Catenate      -> catenateMachine
+    RangeLow  l   -> atLeastMachine l
+    RangeHigh h   -> atMostMachine h
+    RangeBand l h -> bandMachine l h
+    Optional      -> optionalMachine
+    LParen        -> pure
+    RParen        -> pure
+    Class _       -> pure
+    NegClass _    -> pure
+
+alternateMachine :: FragmentStack -> Failing FragmentStack
+alternateMachine stack = case stack of
+  s2:s1:rest -> pure $ (Branch <$> s1 <*> s2):rest
+  _          -> errAlternate <$$> "alternateMachine"
+
+catenateMachine :: FragmentStack -> Failing FragmentStack
+catenateMachine stack = case stack of
+  s2:s1:rest -> pure $ (s1 . s2):rest
+  _          -> errAlternate <$$> "catenateMachine"
+
+repeatFragment :: Int -> MachineFragment -> MachineFragment
+repeatFragment 0 _ = id
+repeatFragment n s = s . repeatFragment (n-1) s
+
+atLeastMachine :: Int -> FragmentStack -> Failing FragmentStack
+atLeastMachine n stack = case stack of
+  s:rest -> pure $ (manyMachine s . repeatFragment n s):rest
+  _      -> errRepeat <$$> "atLeastMachine"
+
+atMostMachine :: Int -> FragmentStack -> Failing FragmentStack
+atMostMachine n stack = case stack of
+  s:rest -> pure $ repeatFragment n (optionalFragment s) : rest
+  _      -> errRepeat <$$> "atMostMachine"
+
+manyMachine :: MachineFragment -> MachineFragment
+manyMachine s = \f -> fix (Branch f . s)
+
+bandMachine :: Int -> Int -> FragmentStack -> Failing FragmentStack
+bandMachine _ _ []          = errRepeat <$$> "bandMachine"
+bandMachine low high (f:fs) = case compare low high of
+  LT -> pure $ (repeatFragment (high - low) (optionalFragment f) . repeatFragment low f) : fs
+  EQ -> pure $ repeatFragment low f : fs
+  GT -> errRange <$$> "bandMachine"
+
+optionalMachine :: FragmentStack -> Failing FragmentStack
+optionalMachine stack = case stack of
+  s:rest -> pure $ optionalFragment s : rest
+  _      -> errRepeat <$$> "optionalMachine"
+
+optionalFragment :: MachineFragment -> MachineFragment
+optionalFragment s = Branch <$> s <*> id
+
+compileRegex :: String -> Failing RegexState
+compileRegex = shuntRegex >=> fromPostfix
+
+match :: RegexState -> String -> Bool
+match r s = matchRec . map (\x -> (x,r)) $ suffixes s
+
+suffixes :: String -> [String]
+suffixes = scanr (:) []
+
+succeed :: (a, RegexState) -> Bool
+succeed (_, Success) = True
+succeed _            = False
+
+matchRec :: [(String, RegexState)] -> Bool
+matchRec states = case states of
+  [] -> False
+  _  -> if any succeed states
+        then True
+        else matchRec (states >>= advanceState)
+
+advanceState :: (String, RegexState) -> [(String, RegexState)]
+advanceState (s, Success) = pure (s, Success)
+advanceState (s, Branch a b) = [(s, a), (s, b)]
+advanceState ("", _) = []
+advanceState (c:cs, Match x r) = if c == x then pure (cs, r) else []
+advanceState (_:cs, MatchAny r) = pure (cs, r)
